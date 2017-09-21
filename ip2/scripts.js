5a66c38623a95b3e651d5a432f2dd5d46ea7e736
Kisseradjo <kisseradjo@gmail.com>
2017-09-21 12:33:37 -0400

    Add all content

--- ip2/scripts.js
+++ ip2/scripts.js
@@ -1,9 +1,8 @@
-/* Open */
 function openNav() {
-  document.getElementById("myNav").style.height = "100%";
+  document.getElementById("myNav").style.width = "100%";
 }
 
-/* Close */
+
 function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
+  document.getElementById("myNav").style.width = "0%";
 }
